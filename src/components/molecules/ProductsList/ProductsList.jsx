989fa2408e45a0e95d838b9d7f84164b4aa936ba
igordmf <igordanielmf@gmail.com>
2021-08-26 22:02:45 -0300

    Adiciona lista de produtos

--- src/components/molecules/ProductsList/ProductsList.jsx
+++ src/components/molecules/ProductsList/ProductsList.jsx
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import ProductCard from '../../atoms/ProductCard';
-import { Container, Content } from './styles';
+import { Container, Content, ProductWrapper } from './styles';
 
-function ProductsList(props) {
-  const { products } = props;
+function ProductsList({ products, homeRef }) {
+  const [page, setPage] = useState(1);
+  const productsDisplayedNum = 9;
+  const firstDisplayed = (page - 1) * productsDisplayedNum;
+  const lastDisplayed = firstDisplayed + productsDisplayedNum;
+  const productsDisplayed = useMemo(() => products
+    .slice(firstDisplayed, lastDisplayed), [products, firstDisplayed, lastDisplayed]);
+
+  useEffect(() => {
+    homeRef.current.scrollIntoView();
+  }, [page, homeRef])
 
   return (
     <Container>
       <Content>
-        {products.map((product) => (
-          <ProductCard
-            product={ product }
-            key={ `${product.name}${product.code_color}` }
-          />
-        ))}
+        <ProductWrapper>
+          {productsDisplayed.map((product) => (
+            <ProductCard
+              product={ product }
+              key={ `${product.name}${product.code_color}` }
+            />
+          ))}
+        </ProductWrapper>
+        <div>
+          <button onClick={ () => setPage((state) => state-1) } disabled={ page === 1 }>Anterior</button>
+          <button
+            onClick={ () => setPage((state) => state+1) }
+            disabled={ page * productsDisplayedNum > products.length }
+          >Próxima</button>
+        </div>
       </Content>
     </Container>
   )
--- src/components/molecules/ProductsList/styles.js
+++ src/components/molecules/ProductsList/styles.js
@@ -1,13 +1,33 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  
 `;
 
 export const Content = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  >div:nth-child(2) {
+    align-items: center;
+    display:flex;
+    justify-content: space-between;
+    width: 180px;
+
+    button {
+      background-color: #e8ebf1;
+      border-radius: 8px;
+      padding: 10px;
+    }
+  }
+`;
+
+export const ProductWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
   gap: 50px;
   justify-content: center;
   margin: 20px auto;
-  max-width: 1200px;
+  max-width: 1800px;
 `;
+
